eba6b1a3b81e00d890fa3a32311f5f993a79f091
SmashAndGrabJob <bfriel@vt.edu>
2019-10-15 19:20:36 -0400

    Update default.rb

--- remediation/cookbooks/security_baseline/recipes/default.rb
+++ remediation/cookbooks/security_baseline/recipes/default.rb
@@ -3,9 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-if node['os'] == 'linux'
-  include_recipe 'os-hardening::default'
-elsif node['os'] == 'windows'
-  include_recipe 'windows-hardening'
-end
 
+if node['os'] == 'linux'
+    include_recipe 'os-hardening::default'
+elsif node['os'] == 'windows'
+    include_recipe 'windows-hardening'
+end
