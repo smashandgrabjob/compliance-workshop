c0e03243e92aaecda647f20f9212cab87a138a18
SmashAndGrabJob <bfriel@vt.edu>
2019-10-15 19:26:09 -0400

    Update plan.sh

--- remediation/habitat/plan.sh
+++ remediation/habitat/plan.sh
@@ -1,7 +1,7 @@
 scaffold_policy_name="base"
-pkg_name=remediation
-pkg_origin=cheftv
-pkg_version="0.2.0"
+pkg_name=base
+pkg_origin=roadtrip
+pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The Chef $scaffold_policy_name Policy"
 pkg_upstream_url="http://chef.io"
@@ -10,5 +10,3 @@ pkg_svc_user=("root")
 pkg_exports=(
   [chef_client_ident]="chef_client_ident"
 )
-
-
