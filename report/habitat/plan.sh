07d154d0debfb7a9c0ac60016f600977165dc85f
root <root@ip-172-31-27-161.ec2.internal>
2019-10-16 00:16:19 +0000

    Updates from Reston Learn Chef Rally Roadtrip

--- report/habitat/plan.sh
+++ report/habitat/plan.sh
@@ -1,10 +1,10 @@
-pkg_name=report
-pkg_version=0.2.0
-pkg_origin=cheftv
+pkg_name=audit-baseline
+pkg_version=0.1.0
+pkg_origin=roadtrip
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Effortless Linux Audit Example"
 pkg_scaffolding="chef/scaffolding-chef-inspec"
-scaffold_automate_server_url='https://seattle-roadtrip.workshops.learn.chef.io'
-scaffold_automate_user='admin'
-scaffold_automate_token='5EpysodiqFenQjl62dhLYlZNews='
+scaffold_automate_server_url='https://roadtrip-reston.workshops.learn.chef.io'
+scaffold_automate_user='user7'
+scaffold_automate_token='7wFsUpGqSA4c0wd1i54KDan_WnU='
